0d2c33e0957e09988137ade488a2f1f3157acfb3
Superkid1235 <supakit.developer@gmail.com>
2022-06-04 00:08:22 +0700

    static method

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
-class car:
-    # create class variables
-    vehicle = "car"
+class Employee:
 
-    def __init__(self, brand, model):
-        # initialize instance variables
-        self.brand = brand
-        self.model = model
+    def __init__(self, name, title, experience):
+        self.name = "HAHA name"
+        self.title = "HAHA title"
+        self.experience = "haha experience"
 
+    def get_experience(self):
+        return self.experience
 
-c1 = car("Toyota", "Altist")
-print("Class Variable  = ", c1.vehicle)
-print("Instance Variable = ", c1.brand, c1.model)
-
-
+    # static method, invoked by a class, not an instance
+    def class_detials():
+        print("This is the parent class")
 
 
+class Engineer(Employee):
+    pass
+
+eng1 = Engineer("Mr.Tim", "Static Eq. Engineer", 2.5)
+
+
+Employee.class_detials()
+Engineer.class_detials()
